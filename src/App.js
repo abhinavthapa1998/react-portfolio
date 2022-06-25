70f5c67995b8749bb64a55688866056c4829c815
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-06-26 00:43:38 +0530

    Sidebar Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.scss";
-
+import Layout from "./components";
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+      </Routes>
+    </>
   );
 }
