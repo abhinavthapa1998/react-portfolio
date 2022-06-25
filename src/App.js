387918be75fbf299a8fdd8f90877e5ded68ba578
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-06-26 01:19:52 +0530

    Home Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.scss";
-import Layout from "./components";
+import Layout from "../src/components/Layout";
+import Home from "./components/Home";
 export default function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />} />
+        <Route index element={<Home />} />
       </Routes>
     </>
   );
